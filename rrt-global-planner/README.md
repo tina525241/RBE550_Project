18f87f4c1c5bab0c5fd5ad51307f8e9a12fd8782
DemargioG <demargiod.g@gmail.com>
2022-04-30 00:41:40 -0400

    Rrt Star Planner
    I've added some edits I made to the RRT Global Planner I found. I've changed it from an RRT planner to an RRT* Planner. Currently it has a problem and is getting stuck finding a random free node.
    
    I'll add the multi goal aspect but, I also want to make sure the RRT* works.

---
+++ rrt-global-planner/README.md
@@ -0,0 +1,59 @@
+# rrt-global-planner
+A [Rapidly Exploring Random Trees (RRT)](https://en.wikipedia.org/wiki/Rapidly-exploring_random_tree) global path planner plugin for ROS.
+
+## Demo
+![](https://github.com/mech0ctopus/rrt-global-planner/raw/main/assets/rrt_tb3_rviz.gif)
+
+## Install Dependancies and Build
+```bash
+cd ~/catkin_ws/src
+git clone https://github.com/mech0ctopus/rrt-global-planner.git
+cd .. && rosdep install --from-paths src --ignore-src -r -y
+catkin_make
+source devel/setup.bash
+```
+
+## Usage
+Within the [move\_base](https://wiki.ros.org/move_base) node in your launch file, set the `base_global_planner` parameter to `global_planner/RRTGlobalPlanner` and load the required parameters.
+```xml
+<param name="base_global_planner" value="global_planner/RRTGlobalPlanner"/>
+<rosparam file="$(find rrt-global-planner)/params/rrt_global_planner.yaml" command="load" />
+```
+
+After launching the system, when you set a `move_base/goal` using RViz's `2D Nav Goal` or with an action client, the `RRTGlobalPlanner` will be called. The global path will be published as a topic for visualization. Optionally, a visualization of the full RRT constructed for path planning will be published.
+
+`RRTGlobalPlanner`'s output can be visualized in RViz. To see the global path, add a `Path` display and subscribe to `~/move_base/RRTGlobalPlanner/plan`. To see the full tree (`viz_tree` must be true), add a `Marker` display and subscribe to `~/move_base/RRTGlobalPlanner/tree`.
+
+Open the amcl.launch file and change the default coordinates to -2.0 for "x" and -0.5 for "y". 
+
+## Examples
+An example launch file for using `RRTGlobalPlanner` with the [TurtleBot3 Simulation](https://emanual.robotis.com/docs/en/platform/turtlebot3/simulation/) is located in [rrt-global-planner/launch](https://github.com/mech0ctopus/rrt-global-planner/tree/main/launch).
+
+An example RViz config file for using `RRTGlobalPlanner` with the [TurtleBot3 Simulation](https://emanual.robotis.com/docs/en/platform/turtlebot3/simulation/) is located in [rrt-global-planner/config](https://github.com/mech0ctopus/rrt-global-planner/tree/main/config).
+
+## ROS API
+### Published Topics
+`~/move_base/RRTGlobalPlanner/plan` ([nav\_msgs/Path](http://docs.ros.org/api/nav_msgs/html/msg/Path.html))
+- The global path constructed by the planner. Used for visualization purposes.
+
+`~/move_base/RRTGlobalPlanner/tree` ([visualization\_msgs/Marker](http://docs.ros.org/en/api/visualization_msgs/html/msg/Marker.html))
+- Visualization of full tree built during planning process.
+
+### Subscribed Topics
+None.
+
+### Parameters
+`~/move_base/RRTGlobalPlanner/goal_tol` (`double`, default: 0.05)
+- Cartesian goal tolerance to be achieved by the global planner.
+
+`~/move_base/RRTGlobalPlanner/K_in` (`int`, default: 4000)
+- Maximum number of iterations to attempt to find a plan.
+
+`~/move_base/RRTGlobalPlanner/d` (`double`, default: 0.2)
+- Distance to extend tree per iteration.
+
+`~/move_base/RRTGlobalPlanner/viz_tree` (`bool`, default: false)
+- Whether to publish full tree on `~/move_base/RRTGlobalPlanner/tree` topic for visualization purposes after planning success. 
+
+### Services
+None.
---
+++ rrt-global-planner/include/2d_spaceStar.hpp
@@ -0,0 +1,211 @@
+#pragma once
+
+#include <math.h>
+#include <random>
+
+#include <costmap_2d/costmap_2d_ros.h>
+#include <geometry_msgs/Point.h>
+
+#include "math_funcs.hpp"
+
+#define TWO_M_PI 2 * M_PI
+#define M_PI_10 M_PI / 10.
+
+/**
+ *  @brief Calculates Euclidean distance between two 2D points.
+ *
+ *  @details
+ *   Calculates Euclidean distance between two 2D points.
+ *
+ *  @param distance L2 Norm.
+ *  @param point1 First point.
+ *  @param point2 Second point.
+ *  @return L2 Norm.
+ *
+ */
+double getDistance(const geometry_msgs::Point point1, const geometry_msgs::Point point2)
+{
+  double distance = sqrt(pow(point2.y - point1.y, 2) + pow(point2.x - point1.x, 2));
+  return distance;
+}
+
+/**
+ *  @brief Checks whether a point is free on global 2D costmap.
+ *
+ *  @details
+ *   Checks point in global 2D costmap to confirm that the robot
+ *   can be placed in any orientation and will only occupy free space.
+ *
+ *  @param point Two points representing a line.
+ *  @param costmap_ros  Pointer to ROS wrapper for global 2D costmap.
+ *  @param robot_radius_max Circumscribed and padded robot radius.
+ *  @return                 Whether point is in free space.
+ *
+ */
+bool inFreeSpace(const geometry_msgs::Point point, const costmap_2d::Costmap2DROS* costmap_ros,
+                 const double robot_radius_max)
+{
+  bool result{ 1 };
+  double theta{ 0 };
+  double robot_radius_ii{ robot_radius_max };
+  double robot_radius_step(0.05);  // Assume 5cm step.
+  costmap_2d::Costmap2D* costmap_;
+  geometry_msgs::Point point_to_check;
+  unsigned int mx, my;
+  std::vector<costmap_2d::MapLocation> map_polygon, polygon_cells;
+
+  costmap_ = costmap_ros->getCostmap();
+
+  // Build inflated/circumscribed robot footprint
+  while (theta <= TWO_M_PI)
+  {
+    costmap_2d::MapLocation map_loc;
+
+    // Try to convert footprint to map coordinates
+    if (!costmap_->worldToMap(point.x + robot_radius_max * cos(theta), point.y + robot_radius_max * sin(theta),
+                              map_loc.x, map_loc.y))
+    {
+      // ROS_INFO("Footprint point is outside of map bounds.");
+      return false;
+    }
+
+    map_polygon.push_back(map_loc);
+
+    theta += M_PI_10;
+  }
+
+  // Get the all map cells within inflated/circumscribed robot footprint
+  costmap_->convexFillCells(map_polygon, polygon_cells);
+
+  // For each cell in polygon_cells, check the cost against the threshold.
+  for (unsigned int i = 0; i < polygon_cells.size(); ++i)
+  {
+    if (costmap_->getCost(polygon_cells[i].x, polygon_cells[i].y) >= costmap_2d::INSCRIBED_INFLATED_OBSTACLE)
+    {
+      result = 0;
+      break;
+    }
+  }
+
+  return result;
+}
+
+/**
+ *  @brief Checks whether an edge is free on global 2D costmap.
+ *
+ *  @details
+ *   Discretizes edge, then checks several points along edge on global 2D costmap
+ *   to confirm that the robot can be placed in any orientation and
+ *   will only occupy free space.
+ *
+ *  @param edge Two points representing a line.
+ *  @param costmap_ros  Pointer to ROS wrapper for global 2D costmap.
+ *  @param robot_radius Circumscribed and padded robot radius.
+ *  @return             Whether edge is in free space.
+ *
+ */
+bool edgeInFreeSpace(const std::vector<geometry_msgs::Point> edge, const costmap_2d::Costmap2DROS* costmap_ros,
+                     const double robot_radius)
+{
+  bool result{ 1 };
+
+  // Discretize edge into an array of points
+  double dist = getDistance(edge[0], edge[1]);
+  // Get num of points. radius acts as resolution.
+  double num_points = dist / robot_radius;
+  geometry_msgs::Point edge_pt_ii{};
+  for (double ii = 0.; ii <= num_points; ii++)
+  {
+    edge_pt_ii.x = edge[0].x + ii * (edge[1].x - edge[0].x) / num_points;
+    edge_pt_ii.y = edge[0].y + ii * (edge[1].y - edge[0].y) / num_points;
+
+    if (!inFreeSpace(edge_pt_ii, costmap_ros, robot_radius))
+    {
+      result = 0;
+      break;
+    }
+  }
+
+  return result;
+}
+
+/**
+ *  @brief Picks a random interger on a uniform distribution.
+ *
+ *  @details
+ *   Picks a random interger to be compared to the bias. If the
+ *   random number chosen is less than or equal to the bias,
+ *   the goal state is chosen as our "random" state.
+
+ *  @see   https://stackoverflow.com/questions/5008804/generating-random-integer-from-a-range
+ *
+ *  @param bias The percentage chance of picking the goal state.
+ *  @return             If the chosen point should be bias (the Goal State).
+ *
+ */
+bool isBias(const int bias)
+{
+
+  std::random_device rd;     // only used once to initialise (seed) engine
+  std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
+  std::uniform_int_distribution<int> uni(1,100); // guaranteed unbiased
+
+  int random_integer = uni(rng);
+
+  ROS_INFO("The random bias is %i.",random_integer); 
+
+  if (random_integer <= bias)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+/**
+ *  @brief Picks a random state on global 2D costmap.
+ *
+ *  @details
+ *   Picks a random point on global 2D costmap at which the robot can be placed
+ *   in any orientation and will only occupy free space.
+ *
+ *  @param costmap_ros  Pointer to ROS wrapper for global 2D costmap.
+ *  @param robot_radius Circumscribed and padded robot radius.
+ *  @param bias The percentage chance of picking the goal state.
+ *  @param goalState the goal state of the search.
+ *  @return             Random point on global costmap where robot is in free space.
+ *
+ */
+geometry_msgs::Point getRandomState(costmap_2d::Costmap2DROS* costmap_ros, const double robot_radius, const int bias, const geometry_msgs::Point* goalState)
+{
+  geometry_msgs::Point randomState{};
+  randomState.z = 0.;  // Assume z=0 for now.
+  costmap_2d::Costmap2D* costmap_;
+  costmap_ = costmap_ros->getCostmap();
+
+  // Keep picking points until you find one in free space
+  bool pointIsFree{ 0 };
+
+  double origin_x = costmap_->getOriginX();
+  double origin_y = costmap_->getOriginY();
+
+  while (!pointIsFree)
+  {
+    // Bias Check. Should we pick the goal position?
+    if (isBias(bias))
+    {
+      randomState.x = goalState->x;
+      randomState.y = goalState->y;
+    }
+    else
+    {
+      randomState.x = randomDouble(origin_x, origin_x + costmap_->getSizeInMetersX());
+      randomState.y = randomDouble(origin_y, origin_y + costmap_->getSizeInMetersY());
+    }
+    ROS_INFO("Check the point!"); 
+    pointIsFree = inFreeSpace(randomState, costmap_ros, robot_radius);
+  }
+  return randomState;
+}
